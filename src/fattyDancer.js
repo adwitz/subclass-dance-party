0f3f517880829aa23a07fd6ec529ff8b69bd94ac
adwitz <adam.witzel@gmail.com>
2013-10-01 17:44:37 -0700

    refactor, remove commented test lines

--- src/fattyDancer.js
+++ src/fattyDancer.js
@@ -1,7 +1,6 @@
 var FattyDancer = function(top, left, timeBetweenSteps){
   Dancer.apply(this, arguments);
   this.$node.addClass('thin').removeClass('dancer');
-  this.expanding = true;
 };
 
 FattyDancer.prototype = Object.create(Dancer.prototype);
@@ -10,15 +9,6 @@ FattyDancer.prototype.constructor = FattyDancer;
 FattyDancer.prototype.step = function(){
   Dancer.prototype.step.apply(this);
   var that = this.$node;
-  /*var expand = function(){
-    var flag = this.expanding ? 1 : -1;
-    this.expanding = !this.expanding;
-    that.animate({"width": '-='+(flag*100),
-    "height": '-='+(flag*100),
-    "left": "-="+(flag*30),
-    "right": "-="+(flag*30)}, 1000);
-    that.toggleClass('fatty');
-  }();*/
   that.toggleClass('fatty');
 };
 
